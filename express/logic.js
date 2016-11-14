d9e84e27851b7383e1437a132dc60af69a9448c1
Samier Saeed <samier_saeed@alumni.brown.edu>
2016-11-13 18:19:46 -0800

    switch to using file db

--- express/logic.js
+++ express/logic.js
@@ -1,5 +1,13 @@
+var knex = require('knex')({
+    client: 'sqlite3',
+    connection: {
+        filename: "./db.sqlite"
+    }
+});
+
+
 module.exports = {
-    create: function(payload, cb){
+    create: function (payload, cb) {
         //do something with the payload, like insert to db
         cb();
     }
--- koa/logic.js
+++ koa/logic.js
@@ -1,3 +1,12 @@
+import knex from "knex";
+
+const conn = knex({
+    client: 'sqlite3',
+    connection: {
+        filename: "./db.sqlite"
+    }
+});
+
 export default {
     create: function (payload) {
         //do something with the payload, like insert to db. 
